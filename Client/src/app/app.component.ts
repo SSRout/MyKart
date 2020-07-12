1c40481405fa825e711c07acf1a173f2c936e36c
Rocky <smruti.sambit.rout@gmail.com>
2020-07-13 00:00:21 +0530

    navbar added

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyKart';
+  Products:IProduct[];
+  constructor(private http : HttpClient) {}
+
+  ngOnInit():void{
+    this.http.get("https://localhost:5001/api/products").subscribe((response:IPagination)=>{
+     this.Products=response.data;
+    },error=>{
+      console.log(error);
+    });
+  }
 }
